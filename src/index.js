d90710d0557f0584e63f26c21d99f00c41b41967
Luiza Petrinina <petrinina95@gmail.com>
2025-01-16 17:31:34 +0200

    Add main layout

--- src/index.js
+++ src/index.js
@@ -12,8 +12,11 @@ app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
 
+
+
 app.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
 });
+
 
 app.listen(5000, () => console.log('Server is listenig on http://localhost:5000... '));
