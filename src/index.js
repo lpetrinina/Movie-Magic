e94c640cec028ad48fa1cd0fa1f042090aad8c30
Luiza Petrinina <petrinina95@gmail.com>
2025-01-16 15:51:07 +0200

    Add and set up Handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('It works!');
+    res.render('home', { layout: false });
 });
 
 app.listen(5000, () => console.log('Server is listenig on http://localhost:5000... '));
