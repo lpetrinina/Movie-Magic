ecc7e38bcadf836eb58ba913b2a16bb544531751
Luiza Petrinina <petrinina95@gmail.com>
2025-01-17 12:37:26 +0200

    Add Details page with hardcoded markup

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -4,6 +4,10 @@ const movieControler = Router();
 
 movieControler.get('/create', (req, res) => {
     res.render('create');
+});
+
+movieControler.get('/:movieId/details', (req, res) => {
+    res.render('details');
 })
 
 export default movieControler;
