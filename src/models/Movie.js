a32b62571ecf6868a4a669d3dc2d2e6685c4c22a
Luiza Petrinina <petrinina95@gmail.com>
2025-01-23 13:10:26 +0200

    Create movie model

---
+++ src/models/Movie.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+
+
+// Create schema
+const movieSchema = new Schema({
+    title: String,
+    category: String,
+    genre: String,
+    director: String,
+    year: Number,
+    rating: Number,
+    description: String,
+    imageURL: String,
+});
+
+// Create model
+const Movie = model('Movie', movieSchema);
+
+export default Movie;
