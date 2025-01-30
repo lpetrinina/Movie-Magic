5f1f713f048364031a94e1feb2eeb734b35b9163
Luiza Petrinina <petrinina95@gmail.com>
2025-01-30 15:41:30 +0200

    Hash password on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,20 @@
 import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
+
 
 const userSchema = new Schema({
     email: String,
     password: String
 });
+
+// Hash pasword before save it
+userSchema.pre('save', async function () {
+
+    // TODO: fix update user bug
+
+    // get plain pass and generate hash with 10 rounds
+    this.password = await bcrypt.hash(this.password, 10);
+})
 
 const User = model('User', userSchema);
 
