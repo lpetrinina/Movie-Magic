3ed460f06dbffdf6243d3b8862e49a88875e4718
Luiza Petrinina <petrinina95@gmail.com>
2025-01-30 16:25:33 +0200

    Add login functionality with jwt generation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -18,6 +18,24 @@ authController.post('/register', async (req, res) => {
 
 authController.get('/login', (req, res) => {
     res.render('auth/login');
-})
+});
+
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+
+        const token = await authServise.login(email, password);
+        console.log(token);
+
+
+    } catch (error) {
+
+        console.log(error.message);
+        return res.redirect('/404')
+    }
+
+    res.redirect('/');
+});
 
 export default authController;
--- src/services/auth-servise.js
+++ src/services/auth-servise.js
@@ -1,4 +1,8 @@
 import User from "../models/User.js";
+import bcrupt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'frJwpXsbepe37iSkoMwGLDrosHcsOLHD64ndyKqe9nqA5PJhd02YucJ4pJke';
 
 export default {
 
@@ -6,6 +10,35 @@ export default {
 
         // Create new user by User model in database
         return User.create(userData);
+    },
+
+    async login(email, password) {
+
+        // Check if the user exists in database
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new Error('Invalid email or password!');
+        }
+
+        // Chech if the password is correct 
+        const isValid = await bcrupt.compare(password, user.password);
+
+        if (!isValid) {
+            throw new Error('Invalid email or password!');
+        }
+
+        // Generate JWT token
+        const payload = {
+            id: user._id,
+            email: user.email
+        };
+
+        // TODO: use async option
+        const token = jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
+        // return the generated token
+        return token;
 
     }
 }
