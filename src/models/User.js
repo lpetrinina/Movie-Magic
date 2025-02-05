514dfceebfbb3835d84e8e2857d3172dd6ac5e70
Luiza Petrinina <petrinina95@gmail.com>
2025-02-05 16:14:05 +0200

    Add user model validation

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,19 @@ import bcrypt from 'bcrypt';
 
 
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        required: [true, 'Email is required!'],
+        unique: true,
+        match: /\@[a-zA-Z]+.[a-zA-Z]+$/,
+        minLength: 10,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!'],
+        match: [/[a-zA-z-0-9]+/, 'Password should be alphanumeric and digits only!'],
+        minLength: [6, 'Password should be at least 6 characters! long'],
+    },
 });
 
 // Hash pasword before save it
