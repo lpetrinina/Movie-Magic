863743e6374c219bb03da72cd40bc988f977fcb4
Luiza Petrinina <petrinina95@gmail.com>
2025-02-05 14:01:15 +0200

    Add movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -4,14 +4,48 @@ import { Schema, model, Types } from "mongoose";
 
 // Create schema
 const movieSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: [true, 'Title is required!'],
+        minLength: [5, 'Title should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Title should be alphanumeric, digits, and whitespaces only!'], //English letters, digits, and whitespaces 
+    },
     category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    rating: Number,
-    description: String,
-    imageUrl: String,
+    genre: {
+        type: String,
+        required: [true, 'Genre is required!'],
+        minLength: [5, 'Genre should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Genre should be alphanumeric, digits, and whitespaces only!'],
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required!'],
+        minLength: [5, 'Director should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Director should be alphanumeric, digits, and whitespaces only!'],
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required!'],
+        min: 1900,
+        max: 2025,
+    },
+    rating: {
+        type: Number,
+        required: [true, 'Rating is required!'],
+        min: 1,
+        max: 10,
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required!'],
+        minLength: [20, 'Description should be at least 20 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Description should be alphanumeric, digits, and whitespaces only!'],
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image is required!'],
+        match: /^https?:\/\//, //url should start with http://... or https://...
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
