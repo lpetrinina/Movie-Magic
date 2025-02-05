ae23a6aeb40f48bdc56ca26f9d25f4fe7d4fb1b5
Luiza Petrinina <petrinina95@gmail.com>
2025-02-05 15:50:04 +0200

    Add cast model validation

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,17 +1,29 @@
 import { Schema, model } from "mongoose";
 
-// •	name – String, required 
-// •	age – Number, required, max and min value 
-// •	born – String, required 
-// •	name in movie – String, required 
-// •	cast image – String, required, http/https validation 
-// •	movie – ObjectId, ref Movie Model 
 
 const castSchema = new Schema({
-    name: String,
-    age: Number,
-    born: String,
-    imageUrl: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name should be at least 5 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Name should be alphanumeric, digits, and whitespaces only!'],
+    },
+    age: {
+        type: Number,
+        min: 1,
+        max: 120
+    },
+    born: {
+        type: String,
+        required: [true, 'Born is required!'],
+        minLength: [10, 'Born should be at least 10 characters long!'],
+        match: [/^[a-zA-Z0-9 ]+$/, 'Born should be alphanumeric, digits, and whitespaces only!'],
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image is required!'],
+        match: /^https?:\/\//, //url should start with http://... or https://...
+    },
 });
 
 const Cast = model('Cast', castSchema);
